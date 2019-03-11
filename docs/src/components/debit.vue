d48a0d9aeebe5fae22715e83a0da6d4526fa441b
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-03-11 18:59:07 -0300

    :blue_book: Correção da aba de débito

--- docs/src/components/debit.vue
+++ docs/src/components/debit.vue
@@ -1,58 +1,62 @@
 <template>
-  <div class="container">
-    <auttar-form-number
-      v-model.numeric="amount"
-      label="Valor"
-      placeholder="Valor que será processado"
-    />
-    <auttar-form-number
-      v-model.numeric="installment"
-      label="Parcelas"
-      placeholder="Quantidade de parcelas"
-    />
-    <auttar-form-toggle
-      v-model="interest"
-      label="Juros"
-      placeholder="Juros pela operadora"
-    />
-    <div class="columns">
-      <div class="column">
-        <button
-          class="button is-danger"
-          @click="reset"
-        >
-          Reiniciar
-        </button>
-      </div>
-      <div class="column has-text-right">
-        <button
-          class="button is-primary"
-          style="margin-right: 20px;"
-          @click="start"
-        >
-          Iniciar
-        </button>
-        <button
-          class="button is-success"
-          @click="confirm"
-        >
-          Confirmar
-        </button>
+  <div class="columns">
+    <div class="column">
+      <auttar-form-text
+        v-model="orderId"
+        :maxlength="10"
+        label="Order ID"
+        placeholder="ID da ordem que vai ser enviado para a Auttar"
+      />
+      <auttar-form-number
+        v-model.numeric="amount"
+        label="Valor"
+        placeholder="Valor que será processado"
+      />
+      <auttar-form-toggle
+        v-model="voucher"
+        label="Voucher"
+        placeholder="Utilizando um voucher para pagamento"
+      />
+      <div class="columns">
+        <div class="column">
+          <button
+            class="button is-danger"
+            @click="reset"
+          >
+            Reiniciar
+          </button>
+        </div>
+        <div class="column has-text-right">
+          <button
+            class="button is-primary"
+            style="margin-right: 20px;"
+            @click="start"
+          >
+            Iniciar
+          </button>
+          <button
+            class="button is-success"
+            @click="confirm"
+          >
+            Confirmar
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
   import AuttarFormNumber from './form/number';
+  import AuttarFormText from './form/text';
   import AuttarFormToggle from './form/toggle';
 
   export default {
     name: 'AuttarDebit',
-    components: { AuttarFormToggle, AuttarFormNumber },
+    components: { AuttarFormText, AuttarFormToggle, AuttarFormNumber },
     data: () => ( {
+      orderId: '',
       amount: 0,
-      installment: 1,
-      interest: false,
+      voucher: false,
       started: false,
     } ),
     methods: {
@@ -60,30 +64,44 @@
         if (!this.amount) return false;
         this.$dialog.confirm({
                                title: 'Iniciar Transação',
-                               message: `Você está iniciando uma compra no cartão de crédito.<br /><br />
-                                <strong>Valor</strong> R$ ${this.amount} ${this.installment > 1 ? `em ${this.installment}x` : ''}<br />
-                                ${this.interest ? '<br /><strong>Juros</strong> pela operadora.' : ''}`,
+                               message: `Você está iniciando uma compra no cartão de débito.<br /><br />
+                                <strong>Valor</strong> R$ ${this.amount}<br />
+                                ${this.voucher ? '<br /><strong>Voucher</strong> send utilizado.' : ''}`,
                                cancelText: 'Cancelar',
                                confirmText: 'Iniciar',
                                type: 'is-success',
                                onConfirm: () => {
                                  this.$toast.open('Cheque o TEF.');
                                  this.$emit('start', {
+                                   type: 'debit',
+                                   orderId: this.orderId,
                                    amount: this.amount,
-                                   installment: this.installment,
-                                   interest: this.interest,
+                                   voucher: this.voucher,
                                  });
                                  this.started = true;
                                }
                              });
       },
       confirm() {
-        if (this.started) this.$emit('confirm');
+        if (this.started) {
+          this.$dialog.confirm({
+                                 title: 'Confirmar Transação',
+                                 message: 'Você tem certeza que deseja confirmar a transação?',
+                                 confirmText: 'Confirmar',
+                                 type: 'is-danger',
+                                 hasIcon: true,
+                                 onConfirm: () => {
+                                   this.$emit('confirm');
+                                   this.reset();
+                                   this.$toast.open('Transação confirmada');
+                                 }
+                               })
+        }
       },
       reset() {
+        this.orderId = '';
         this.amount = 0;
-        this.installment = 1;
-        this.interest = false;
+        this.voucher = false;
         this.started = false;
         this.$emit('reset');
       },
