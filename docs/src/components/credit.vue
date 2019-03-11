c8a6a3db8c5a22f84428b657f253fcd8fad71695
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-03-11 18:59:00 -0300

    :blue_book: Correção da aba de crédito

--- docs/src/components/credit.vue
+++ docs/src/components/credit.vue
@@ -1,55 +1,65 @@
 <template>
-  <div class="container">
-    <auttar-form-number
-      v-model.numeric="amount"
-      label="Valor"
-      placeholder="Valor que será processado"
-    />
-    <auttar-form-number
-      v-model.numeric="installment"
-      label="Parcelas"
-      placeholder="Quantidade de parcelas"
-    />
-    <auttar-form-toggle
-      v-model="interest"
-      label="Juros"
-      placeholder="Juros pela operadora"
-    />
-    <div class="columns">
-      <div class="column">
-        <button
-          class="button is-danger"
-          @click="reset"
-        >
-          Reiniciar
-        </button>
-      </div>
-      <div class="column has-text-right">
-        <button
-          class="button is-primary"
-          style="margin-right: 20px;"
-          @click="start"
-        >
-          Iniciar
-        </button>
-        <button
-          class="button is-success"
-          @click="confirm"
-        >
-          Confirmar
-        </button>
+  <div class="columns">
+    <div class="column">
+      <auttar-form-text
+        v-model="orderId"
+        :maxlength="10"
+        label="Order ID"
+        placeholder="ID da ordem que vai ser enviado para a Auttar"
+      />
+      <auttar-form-number
+        v-model.numeric="amount"
+        label="Valor"
+        placeholder="Valor que será processado"
+      />
+      <auttar-form-number
+        v-model.numeric="installment"
+        label="Parcelas"
+        placeholder="Quantidade de parcelas"
+      />
+      <auttar-form-toggle
+        v-model="interest"
+        label="Juros"
+        placeholder="Juros pela operadora"
+      />
+      <div class="columns">
+        <div class="column">
+          <button
+            class="button is-danger"
+            @click="reset"
+          >
+            Reiniciar
+          </button>
+        </div>
+        <div class="column has-text-right">
+          <button
+            class="button is-primary"
+            style="margin-right: 20px;"
+            @click="start"
+          >
+            Iniciar
+          </button>
+          <button
+            class="button is-success"
+            @click="confirm"
+          >
+            Confirmar
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
   import AuttarFormNumber from './form/number';
+  import AuttarFormText from './form/text';
   import AuttarFormToggle from './form/toggle';
 
   export default {
     name: 'AuttarCredit',
-    components: { AuttarFormToggle, AuttarFormNumber },
+    components: { AuttarFormText, AuttarFormToggle, AuttarFormNumber },
     data: () => ( {
+      orderId: '',
       amount: 0,
       installment: 1,
       interest: false,
@@ -69,7 +79,9 @@
                                onConfirm: () => {
                                  this.$toast.open('Cheque o TEF.');
                                  this.$emit('start', {
+                                   type: 'credit',
                                    amount: this.amount,
+                                   orderId: this.orderId,
                                    installment: this.installment,
                                    interest: this.interest,
                                  });
@@ -78,9 +90,23 @@
                              });
       },
       confirm() {
-        if (this.started) this.$emit('confirm');
+        if (this.started) {
+          this.$dialog.confirm({
+                                 title: 'Confirmar Transação',
+                                 message: 'Você tem certeza que deseja confirmar a transação?',
+                                 confirmText: 'Confirmar',
+                                 type: 'is-danger',
+                                 hasIcon: true,
+                                 onConfirm: () => {
+                                   this.$emit('confirm');
+                                   this.reset();
+                                   this.$toast.open('Transação confirmada');
+                                 }
+                               })
+        }
       },
       reset() {
+        this.orderId = '';
         this.amount = 0;
         this.installment = 1;
         this.interest = false;
