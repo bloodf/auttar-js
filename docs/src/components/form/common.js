451d39bd565bdc92c6cfb906e085d8e307d57572
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-03-11 18:58:42 -0300

    :blue_book: Novos inputs

--- docs/src/components/form/common.js
+++ docs/src/components/form/common.js
@@ -1,5 +1,15 @@
 export default {
   props: {
+    value: {
+      type: [String, Boolean, Array, Object],
+      required: false,
+      default: '',
+    },
+    maxlength: {
+      type: [String, Number],
+      required: false,
+      default: '',
+    },
     label: {
       type: String,
       required: false,
---
+++ docs/src/components/form/text.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="columns">
+    <div class="column">
+      <b-field
+        :label="label"
+        :message="placeholder"
+        horizontal
+      >
+        <b-input
+          :value="value"
+          type="text"
+          :maxlength="maxlength"
+          @input="$emit('input', $event)"
+        />
+      </b-field>
+    </div>
+  </div>
+</template>
+<script>
+  import CommonMixin from './common';
+
+  export default {
+    name: 'AuttarFormText',
+    mixins: [CommonMixin],
+  }
+</script>
